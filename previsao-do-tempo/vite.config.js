17abbd01ef0e922a0ac19685870ebd5b146967ae
BrunoSts94 <bruno_ps94@hotmail.com>
2025-10-16 14:16:52 -0300

    moção de arquivos para pasta docs

--- previsao-do-tempo/vite.config.js
+++ previsao-do-tempo/vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Projetos-React-Portifolio',
-
+  base: '/Projetos-React-Portifolio/previsao-do-tempo',
 })
