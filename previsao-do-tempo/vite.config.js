302dc1942f136a14f315b9a959282b930ffe09cb
BrunoSts94 <bruno_ps94@hotmail.com>
2025-10-27 14:11:28 -0300

    update

--- previsao-do-tempo/vite.config.js
+++ previsao-do-tempo/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Projetos-React-Portifolio/previsao-do-tempo',
+  base: '/Projetos-Com-Build/previsao-do-tempo',
 })
