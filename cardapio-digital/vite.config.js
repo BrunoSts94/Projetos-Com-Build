e35ec912f5cf17492b3d5c4bbd18b1ecef5d0dba
BrunoSts94 <bruno_ps94@hotmail.com>
2025-10-27 13:51:45 -0300

    update diretorio

--- cardapio-digital/vite.config.js
+++ cardapio-digital/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Projetos-React-Portifolio/cardapio-digital/',
+  base: '/Projetos-Com-Build/cardapio-digital/',
 })
