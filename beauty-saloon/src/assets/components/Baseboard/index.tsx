0e4f81e79be9d53a0169941d157301ec25b08cc3
BrunoSts94 <bruno_ps94@hotmail.com>
2025-11-10 13:28:15 -0300

    implementação da sessão sobre e inicio do rodapé

--- beauty-saloon/src/assets/components/Baseboard/index.tsx
+++ beauty-saloon/src/assets/components/Baseboard/index.tsx
@@ -2,8 +2,42 @@ import './style.css'
 
 export function Baseboard(){
     return(
-        <section>
-            
-        </section>
+        <footer>
+            <div className='container-footer-principal'>
+                <div className='first-footer-container'>
+                    <div className='sessao-nome'>
+                        <h2>Thai Estudio</h2>
+                        <p>BEAUTY SALON</p>
+                    </div>
+                    <p>Seu destino privilegiado para tratamentos de beleza avançados. Experimente a combinação perfeita de luxo, experiência e tecnologia de ponta.</p>
+                    <div className='container-social'>
+                        <button></button>
+                    </div>
+                </div>
+
+                <div className='second-footer-container'>
+                    <h4>Serviços</h4>
+                    <ul>
+                        <li><a href="#">Alisamento de cabelo a laser</a></li>
+                        <li><a href="#">Design de Sobrancelhas</a></li>
+                        <li><a href="#">Cuidados posteriores</a></li>
+                    </ul>
+                </div>
+
+                <div className='third-footer-container'>
+                    <h4>Contato</h4>
+                    <ul>
+                        <li><a href="#">123 Beauty Avenida, 1234</a></li>
+                        <li><a href="#">Centro</a></li>
+                        <li><a href="#">+55 (54) 123-4567</a></li>
+                        <li><a href="#">hello@thaiestudio.com</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div className='footer-final-texts'>
+                <p>© 2025 Thai Estudio. All rights reserved. Designed with love for Bruno Santos.</p>
+            </div>
+        </footer>
     )
 }
--- beauty-saloon/src/assets/components/Sections/SectionAbout/index.tsx
+++ beauty-saloon/src/assets/components/Sections/SectionAbout/index.tsx
@@ -1,9 +1,38 @@
 import './style.css'
+import { Button } from '../../Buttons/ButtonDefault'
+import imgSup1 from '../../../img/img2.jpeg'
+import imgSup2 from '../../../img/img3.jpg'
+import imgInf1 from '../../../img/img4.jpg'
+import imgInf2 from '../../../img/service1.jpeg'
 
 export function SectionAbout(){
     return(
-        <section>
-            
+        <section className='section-about'>
+            <div className='container-section-about'>
+                <div className='container-section-about-texts'>
+                    <h2>Sobre a Thai Estudio</h2>
+                    <p>Com mais de uma década de experiência no setor de beleza, o Thai Estudio se tornou o principal destino para tratamentos de beleza avançados na cidade.</p>
+                    <p>Nossa equipe de especialistas certificados utiliza tecnologia de ponta e técnicas comprovadas para oferecer resultados excepcionais. Acreditamos que todos os clientes merecem se sentir confiantes e belos, e estamos comprometidos em tornar essa visão realidade.</p>
+                    <div className='section-about-aditional-infos'>
+                        <div className='info-years'>
+                            <h4>10+</h4>
+                            <p>Anos de experiência</p>
+                        </div>
+                        <div className='info-clients'>
+                            <h4>500+</h4>
+                            <p>Clientes Satisfeitos</p>
+                        </div>
+                    </div>
+                    <Button titulo='Contate-nos' className='btn-about-contact'/>
+                </div>
+
+                <div className='container-section-about-images'> 
+                        <img src={ imgInf1 } className='img-about-inf'/>
+                        <img src={ imgInf2 } className='img-about-inf'/>
+                        <img src={ imgSup1 } className='img-about-sup'/>
+                        <img src={ imgSup2 } className='img-about-sup'/>
+                </div>
+            </div>
         </section>
     )
 }
