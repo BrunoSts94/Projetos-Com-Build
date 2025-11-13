06d6ca79ebeb23aaaa44d93aa8f4bdc23f6e8d92
BrunoSts94 <bruno_ps94@hotmail.com>
2025-11-12 21:49:30 -0300

    Ajustes de responsividade na versão mobile da sessão sobre

--- beauty-saloon/src/assets/components/Sections/SectionAbout/index.tsx
+++ beauty-saloon/src/assets/components/Sections/SectionAbout/index.tsx
@@ -2,39 +2,39 @@ import './style.css'
 import './styleMobile.css'
 
 import { Button } from '../../Buttons/ButtonDefault'
-import imgSup1 from '../../../img/img2.jpeg'
-import imgSup2 from '../../../img/img3.jpg'
-import imgInf1 from '../../../img/img4.jpg'
-import imgInf2 from '../../../img/service1.jpeg'
+import imgSup1 from '../../../img/cabelereira.jpg'
 
 export function SectionAbout(){
     return(
         <section className='section-about'>
             <div className='container-section-about'>
-                <div className='container-section-about-texts'>
-                    <h2>Sobre a Thai Estudio</h2>
-                    <p>Com mais de uma década de experiência no setor de beleza, o Thai Estudio se tornou o principal destino para tratamentos de beleza avançados na cidade.</p>
-                    <p>Nossa equipe de especialistas certificados utiliza tecnologia de ponta e técnicas comprovadas para oferecer resultados excepcionais. Acreditamos que todos os clientes merecem se sentir confiantes e belos, e estamos comprometidos em tornar essa visão realidade.</p>
-                    <div className='section-about-aditional-infos'>
-                        <div className='info-years'>
-                            <h4>10+</h4>
-                            <p>Anos de experiência</p>
-                        </div>
+                <div className='container-section-superior'>
+                    <div className='section-superior-texts'>
+                        <h2>Sobre a Thai Estudio</h2>
+                        <p>Com mais de uma década de experiência no setor de beleza, o Thai Estudio se tornou o principal destino para tratamentos de beleza avançados na cidade.</p>
+                        <p>Nossa equipe de especialistas certificados utiliza tecnologia de ponta e técnicas comprovadas para oferecer resultados excepcionais. Acreditamos que todos os clientes merecem se sentir confiantes e belos, e estamos comprometidos em tornar essa visão realidade.</p>
+
+                        <div className='section-about-aditional-infos'>
+                            <div className='info-years'>
+                                <h4>10+</h4>
+                                <p>Anos de experiência</p>
+                            </div>
                         <div className='info-clients'>
                             <h4>500+</h4>
                             <p>Clientes Satisfeitos</p>
                         </div>
                     </div>
+                    <div className='container-section-inferior'>
                     <Button titulo='Contate-nos' className='btn-about-contact'/>
                 </div>
-
-                <div className='container-section-about-images'> 
-                        <img src={ imgSup1 } className='img-about-sup'/>
-                        <img src={ imgSup2 } className='img-about-sup img-hide-mobile'/>
-                        <img src={ imgInf1 } className='img-about-inf img-hide-mobile'/>
-                        <img src={ imgInf2 } className='img-about-inf img-hide-mobile'/>
-                        
+                    
+                    </div>
+                    <div className='container-about-image'>
+                        <img src={ imgSup1 } alt="" />
+                    </div>
                 </div>
+
+                 
             </div>
         </section>
     )
